bf1cf930687cbb86c45469921168d2307efe3303
Corey Ehmke <cehmke@localoffernetwork.com>
2012-05-01 12:19:10 -0500

    Persist corpus to /tmp

--- lib/bayes_motel/persistence.rb
+++ lib/bayes_motel/persistence.rb
@@ -3,12 +3,12 @@ module BayesMotel
     # TODO Make this a little more Ruby idiomatic and pluggable
     # for filesystems, databases, etc.
     def self.write(corpus)
-      File.open("#{corpus.name}", 'w') do |file|
+      File.open("tmp/#{corpus.name}", 'w') do |file|
         Marshal.dump(corpus, file)
       end
     end
     def self.read(name)
-      Marshal.load(File.read("#{name}"))
+      Marshal.load(File.read("tmp/#{name}"))
     end
   end
 end

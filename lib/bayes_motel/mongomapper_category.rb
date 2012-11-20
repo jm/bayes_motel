b649c8c39a19627f0a85ce807bb203e6d6f0640d
bruno bornsztein <bruno.bornsztein@gmail.com>
2012-11-20 08:34:46 -0600

    remove mongomapper interface

--- lib/bayes_motel/mongomapper_category.rb
+++
@@ -1,9 +0,0 @@
-module BayesMotel
-  module Mongomapper
-    class Category
-      include MongoMapper::Document
-      key :name , String
-      key :classifier_id , :class_name => 'Mongo::ObjectID'
-    end
-  end
-end
--- lib/bayes_motel/mongomapper_classifier.rb
+++
@@ -1,9 +0,0 @@
-module BayesMotel
-  module Mongomapper
-    class Classifier
-      include MongoMapper::Document
-      key :name , String
-      key :total_count , Integer
-    end
-  end
-end
